35999d55bc75c1bab9c610316ceb817dd2d133d4
zieunnn <kindz2@nate.com>
2023-12-06 13:45:19 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,7 +18,17 @@ const login = () => {
     body: JSON.stringify(req),
   })
   .then((res) => res.json())
-  .then(console.log);
+  .then((res) => {
+    if(res.success){
+      location.href = "/";
+    }else{
+      alert(res.msg);
+    }
+  })
+  .catch((err) => {
+    // console.error(new Error("로그인 중 에러 발생!"));
+    console.error("로그인 중 에러 발생!");
+  });
 }
 
 loginBtn.addEventListener("click", login).then;
